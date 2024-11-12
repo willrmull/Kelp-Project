e126e58851d3a032efc9e1ccfc26b4666abfa617
William Mullins <willrmullins96@gmail.com>
2024-11-12 11:14:27 -0600

    Update

--- Urhcins.R
+++ Urhcins.R
@@ -10,12 +10,17 @@ library(readr)
 
 urchin_pred_url = "https://raw.githubusercontent.com/willrmull/Kelp-Project/refs/heads/main/Datasets/byrnes_foodweb_table1%20(1).csv"
 urchin_pred <- read_csv(url(urchin_pred_url))
-                          
+#Alternative
+urchin_pred <- read.csv("~/Documents/Fall 2024/Kelp Project/Kelp-Project/Datasets/byrnes_foodweb_table1 (1).csv")
 ###finds predators of urchin###
 
 urchin_pred <- filter(urchin_pred, Pred_taxon == "Urchins")
 urchin_url = "https://raw.githubusercontent.com/willrmull/Kelp-Project/refs/heads/main/Datasets/Annual_All_Species_Biomass_at_transect_20240823.csv"
 urchin_counts <- read_csv(url(urchin_url))
+
+#Alternative
+urchin_counts<- read.csv("~/Documents/Fall 2024/Kelp Project/Kelp-Project/Datasets/SBS_Urchin_All_Years_20240823.csv")
+
 
 list_urchins <- c(urchin_pred$Predator)
 urchin_counts <- filter(urchin_counts, SCIENTIFIC_NAME %in% list_urchins)
